c1c208aa9b64628863ca333825a46246d35a1084
Filip Barszczewski <f.barszczewski@qt-swiss.eu>
2023-02-19 13:26:05 +0100

    Added empty GLL MWV & Nmea class

---
+++ Autocomp.Nmea.Common/NmeaFormats/GLL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autocomp.Nmea.Common.NmeaFormats
+{
+    public class GLL
+    {
+        public double Latitude { get; private set; }
+        public double Longitude { get; private set; }
+        public TimeSpan UtcTime { get; private set; }
+
+
+    }
+}
---
+++ Autocomp.Nmea.Common/NmeaFormats/MWV.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autocomp.Nmea.Common.NmeaFormats
+{
+    public class MWV
+    {
+        public int Angle { get; private set; }
+        public string Reference { get; private set; }
+        public int Speed { get; private set; }
+        public string Units { get; private set; }
+    }
+}
