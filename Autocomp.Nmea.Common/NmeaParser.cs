127ec2f0ab991145b3167a9609eca7931f4f83e3
Filip Barszczewski <f.barszczewski@qt-swiss.eu>
2023-02-19 13:47:24 +0100

    Added value parsers in NmeaParser

--- Autocomp.Nmea.Common/NmeaParser.cs
+++ Autocomp.Nmea.Common/NmeaParser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,17 +24,38 @@ namespace Autocomp.Nmea.Common
 
         public static double StringToLongitude(string value, string ewIndicator)
         {
-            return 0;
+            if (value == null || value.Length < 3)
+                return double.NaN;
+
+            double longitude = int.Parse(value.Substring(0, 3), CultureInfo.InvariantCulture)
+                             + double.Parse(value.Substring(3), CultureInfo.InvariantCulture) / 60;
+
+            if (ewIndicator == "W")
+                longitude *= -1;
+
+            return longitude;
         }
 
         public static double StringToLatitude(string value, string nsIndicator)
         {
-            return 0;
+            if (value == null || value.Length <2)
+                return double.NaN;
+
+            double latitude = int.Parse(value.Substring(0, 2), CultureInfo.InvariantCulture)
+                            + double.Parse(value.Substring(2), CultureInfo.InvariantCulture) / 60;
+
+            if (nsIndicator == "S")
+                latitude *= -1;
+
+            return latitude;
         }
 
         public static TimeSpan StringToTimeSpan(string value)
         {
-            return new TimeSpan();
+            if (value != null && value.Length < 6)
+                return TimeSpan.Zero;
+
+            return new TimeSpan(int.Parse(value.Substring(0, 2)), int.Parse(value.Substring(2, 2)), int.Parse(value.Substring(4, 2)));
         }
 
         public static int StringToInt(string value)
