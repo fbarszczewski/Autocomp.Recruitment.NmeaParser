1dcfc701c8b19ece3295c3ee3a8a6f84f8cbe5f2
Filip Barszczewski <f.barszczewski@qt-swiss.eu>
2023-02-19 13:34:33 +0100

    added empty methods to NmeaParser class

--- Autocomp.Nmea.Common/NmeaParser.cs
+++ Autocomp.Nmea.Common/NmeaParser.cs
@@ -6,7 +6,34 @@ using System.Threading.Tasks;
 
 namespace Autocomp.Nmea.Common
 {
-    internal class NmeaParser
+    public static class NmeaParser
     {
+
+        public static object Parse(NmeaMessage msg)
+        {
+            return null;
+        }
+
+        public static bool TryParse(NmeaMessage msg, out object result)
+        {
+            result = null;
+
+            return false;
+        }
+
+        public static double StringToLongitude(string value, string ewIndicator)
+        {
+            return 0;
+        }
+
+        public static double StringToLatitude(string value, string nsIndicator)
+        {
+            return 0;
+        }
+
+        public static TimeSpan StringToTimeSpan(string value)
+        {
+            return new TimeSpan();
+        }
     }
 }
