df0d7f0745e2a977ec5da985d7f45d3b67700c3c
Filip Barszczewski <f.barszczewski@qt-swiss.eu>
2023-02-19 14:27:37 +0100

    Added empty console project & Tests project

---
+++ Nmea.Common.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Nmea.Common.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
